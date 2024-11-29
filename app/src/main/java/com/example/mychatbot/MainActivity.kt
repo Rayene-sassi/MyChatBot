a2940bbf9450c6c092a2c68a22cb6d71780b66f3
Mohamed Rayene Sassi <mohamedrayene.sassi2@gmail.com>
2024-11-29 10:01:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mychatbot/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mychatbot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mychatbot.ui.theme.MyChatBotTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val chatViewModel = ViewModelProvider(this)[ChatViewModel::class.java]
+        setContent {
+            MyChatBotTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ChatPage(modifier = Modifier.padding(innerPadding), chatViewModel)
+                }
+
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/mychatbot/MessageModel.kt
@@ -0,0 +1,6 @@
+package com.example.mychatbot
+
+data class MessageModel(
+    val message : String,
+    val role : String,
+)
